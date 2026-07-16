495844f50ad7035dca74caed52947fb4b50283ec
Kadendotcom <kadenbic@buffalo.edu>
2026-07-16 15:29:27 -0400

    the top 5 meals of an ingredient meal list are now listed on with images and names.

--- meal-planner-client/src/App.jsx
+++ meal-planner-client/src/App.jsx
@@ -4,19 +4,30 @@ import './App.css';
 function App() {
 
   const [ingredient, setIngredient] = useState('');
+  const [meals, setMeals] =useState([])
 
   async function handleSubmit(e) {
     e.preventDefault();
     const res = await fetch(`http://localhost:5000/Meals?ingredient=${encodeURIComponent(ingredient)}`);
     const data = await res.json();
-    console.log(data);
+    setMeals((data.meals || []).slice(0,5));
   }
 
   return (
+    <>
     <form onSubmit={handleSubmit}>
       <input value={ingredient} onChange={e => setIngredient(e.target.value)}/>
       <button type="submit">Search</button>
     </form>
+    <ul>
+      {meals.map(meal => (
+        <li key = {meal.idMeal}>
+          <img src={meal.strMealThumb} alt={meal.strMeal}/> 
+          {meal.strMeal}
+          </li>
+      ))}
+    </ul>
+    </>
   );
 }
 export default App;
