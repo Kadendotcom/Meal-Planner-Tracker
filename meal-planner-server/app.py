f0860f7573da2c3ab6b5740735421a6de595a9c9
Kadendotcom <kadenbic@buffalo.edu>
2026-07-16 15:04:13 -0400

    typing a proper meal ingredient into the text box and submitting, returns an object containing meals that contain that ingredient

--- meal-planner-server/app.py
+++ meal-planner-server/app.py
@@ -1,12 +1,16 @@
 import flask
+import requests
 from flask_cors import CORS
 app = flask.Flask(__name__)
 CORS(app)
 
+MEALDB_MEAL_SEARCH_INGREDIENT_URL = 'https://www.themealdb.com/api/json/v1/1/filter.php?i='
 @app.route('/Meals')
 def hello():
     ingredient = flask.request.args.get('ingredient', '')
-    return flask.jsonify(message=f'Searching for {ingredient}')
+    response = requests.get(MEALDB_MEAL_SEARCH_INGREDIENT_URL + ingredient)
+    print(response.json())
+    return flask.jsonify(response.json())
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)      
+    app.run(debug=True, port=5000)  
