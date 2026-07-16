3ca1c7eefabb66cfa11fd8025b40ff232e0a1325
Kadendotcom <kadenbic@buffalo.edu>
2026-07-16 15:56:56 -0400

    Search bar is now alligned to left but search results stay centered. no meals found now displays if no meals were found from the queery

--- meal-planner-client/src/App.jsx
+++ meal-planner-client/src/App.jsx
@@ -5,29 +5,35 @@ function App() {
 
   const [ingredient, setIngredient] = useState('');
   const [meals, setMeals] =useState([])
+  const [searched, setSearched] = useState(false);
 
   async function handleSubmit(e) {
     e.preventDefault();
     const res = await fetch(`http://localhost:5000/Meals?ingredient=${encodeURIComponent(ingredient)}`);
     const data = await res.json();
     setMeals((data.meals || []).slice(0,5));
+    setSearched(true);
   }
 
   return (
     <div className="App">
-    <form onSubmit={handleSubmit}>
+    <form className= "search-form" onSubmit={handleSubmit}>
       <input value={ingredient} onChange={e => setIngredient(e.target.value)}/>
       <button type="submit">Search</button>
     </form>
-    <ul>
-      {meals.map(meal => (
-        <li key = {meal.idMeal}>
-          
-          <img className="meal-thumb" src={meal.strMealThumb} alt={meal.strMeal}/> 
-          {meal.strMeal}
-          </li>
-      ))}
-    </ul>
+    {searched && meals.length === 0 ? (
+      <p>No meals found</p>
+    ) : (
+      <ul>
+        {meals.map(meal => (
+          <li key = {meal.idMeal}>
+
+            <img className="meal-thumb" src={meal.strMealThumb} alt={meal.strMeal}/>
+            {meal.strMeal}
+            </li>
+        ))}
+      </ul>
+    )}
     </div>
   );
 }
