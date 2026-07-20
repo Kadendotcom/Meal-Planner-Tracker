a8704c831c4648b60ac8cd561e6309aa513a2abe
Kadendotcom <kadenbic@buffalo.edu>
2026-07-20 12:07:13 -0400

    Some supporting comments

--- meal-planner-client/src/App.jsx
+++ meal-planner-client/src/App.jsx
@@ -9,8 +9,11 @@ function App() {
 
   async function handleSubmit(e) {
     e.preventDefault();
+    // when a user types an ingredient into the search box (ex. Chicken). react fetches the result of /meals endpoint
+    // api calls mealdb with the suer input ingredient and returns object containing meals and other info on the meals
     const res = await fetch(`http://localhost:5000/Meals?ingredient=${encodeURIComponent(ingredient)}`);
     const data = await res.json();
+    // load the top 5 meals returned (currently alphabetical)
     setMeals((data.meals || []).slice(0,5));
     setSearched(true);
   }
@@ -20,11 +23,13 @@ function App() {
     <form className= "search-form" onSubmit={handleSubmit}>
       <input value={ingredient} onChange={e => setIngredient(e.target.value)}/>
       <button type="submit">Search</button>
+      {/* // if there are no meals returned, return "no meals found" */}
     </form>
     {searched && meals.length === 0 ? (
       <p>No meals found</p>
     ) : (
       <ul>
+        {/* the actual objects that show up on the page. meal name, image, and link.  */}
         {meals.map((meal, index) => (
           <li key = {meal.idMeal} style={{animationDelay: `${index * 80}ms`}}>
             <img className="meal-thumb" src={meal.strMealThumb} alt={meal.strMeal}/>
--- meal-planner-server/app.py
+++ meal-planner-server/app.py
@@ -4,12 +4,16 @@ from flask_cors import CORS
 app = flask.Flask(__name__)
 CORS(app)
 
+# api endpoint
 MEALDB_MEAL_SEARCH_INGREDIENT_URL = 'https://www.themealdb.com/api/json/v1/1/filter.php?i='
 @app.route('/Meals')
 def hello():
+    #fetches the arg from the user input
     ingredient = flask.request.args.get('ingredient', '')
+    #creates a request, api enpoint with an argument of user input ingredient
     response = requests.get(MEALDB_MEAL_SEARCH_INGREDIENT_URL + ingredient)
     print(response.json())
+    # return the response in json 
     return flask.jsonify(response.json())
 
 if __name__ == '__main__':
